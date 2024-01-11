03214b678b668c423e8328f9874b7a2b0df72e74
Marta <martasagun95@gmail.com>
2024-01-11 15:30:21 +0100

    init: initialized marvel project (class components)

---
+++ src/services/MarvelService.js
@@ -0,0 +1,44 @@
+class MarvelService {
+	_apiBase = 'https://gateway.marvel.com:443/v1/public/';
+	_apiKey = 'apikey=eb79b587aaecaff1d5c4dce82b9b6e93';
+
+	getResource = async (url) => {
+		let res = await fetch(url);
+
+		if (!res.ok) {
+			throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+		}
+
+		return await res.json();
+	};
+
+	getAllCharacters = async () => {
+		const res = await this.getResource(
+			`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`
+		);
+		return res.data.results.map(this._transformCharacter);
+	};
+
+	getCharacter = async (id) => {
+		const res = await this.getResource(
+			`${this._apiBase}characters/${id}?${this._apiKey}`
+		);
+		return this._transformCharacter(res.data.results[0]);
+	};
+
+	_transformCharacter = (char) => {
+		return {
+			id: char.id,
+			name: char.name,
+			description: char.description
+				? char.description
+				: 'There is no description for this character.',
+			thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+			homepage: char.urls[0].url,
+			wiki: char.urls[1].url,
+			comics: char.comics.items,
+		};
+	};
+}
+
+export default MarvelService;
